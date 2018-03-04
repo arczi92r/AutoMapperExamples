b46c8bac970551edafb92fb5167b0e371ec4d47e
ArkadiuszT <arczi92r@hotmail.com>
2018-03-04 19:21:16 +0100

    Add project files.

---
+++ ClientConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+namespace ClientConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Register();
+
+
+        }
+        public static void Register()
+        {
+            Mapper.Initialize();
+
+
+        }
+    }
+}
---
+++ DataAccessLayer.Fake/Person.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace DataAccessLayer.Fake
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+
+
+        public IEnumerable<Person> getAllPersons()
+        {
+            List<Person> person = new List<Person>();
+
+            Person p1 = new Person() { Age = 12, Name = "Arek1", Surname = "kjaskdajskd" };
+            Person p2 = new Person() { Age = 14, Name = "Arek2", Surname = "fdgf" };
+            Person p3 = new Person() { Age = 123, Name = "Arek3", Surname = "kfuyi" };
+            Person p4 = new Person() { Age = 19, Name = "Arek4", Surname = "gjghj" };
+            Person p5 = new Person() { Age = 17, Name = "Arek5", Surname = "ddasdsad" };
+
+            person.Add(p1);
+            person.Add(p2);
+            person.Add(p3);
+            person.Add(p4);
+            person.Add(p5);
+
+            return person.AsEnumerable();
+
+        }
+        public async Task<IEnumerable<Person>> getAllPersonsAsync()
+        {
+            List<Person> person = new List<Person>();
+
+            Person p1 = new Person() { Age = 12, Name = "Arek1", Surname = "kjaskdajskd" };
+            Person p2 = new Person() { Age = 14, Name = "Arek2", Surname = "fdgf" };
+            Person p3 = new Person() { Age = 123, Name = "Arek3", Surname = "kfuyi" };
+            Person p4 = new Person() { Age = 19, Name = "Arek4", Surname = "gjghj" };
+            Person p5 = new Person() { Age = 17, Name = "Arek5", Surname = "ddasdsad" };
+
+            person.Add(p1);
+            person.Add(p2);
+            person.Add(p3);
+            person.Add(p4);
+            person.Add(p5);
+
+            return person.AsEnumerable();
+        }
+    }
+
+
+}
