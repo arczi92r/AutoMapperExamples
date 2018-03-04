6b6a82a3195575ad220b09dab39e8515fed08c5b
ArkadiuszT <arczi92r@hotmail.com>
2018-03-04 19:49:27 +0100

    init commit

--- ClientConsole/Program.cs
+++ ClientConsole/Program.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
+using ClientConsole.ViewModel;
+using DataAccessLayer.Fake;
+
 namespace ClientConsole
 {
     class Program
@@ -11,12 +14,24 @@ namespace ClientConsole
         static void Main(string[] args)
         {
             Register();
-
+            Person p = new Person();
+            p.getAllPersons();
+             PersonVM pvm = Mapper.Map<PersonVM>(p);
+            IEnumerable<PersonVM> pvmx = Mapper.Map<IEnumerable<PersonVM>>(p.getAllPersons());
+            foreach (var item in pvmx)
+            {
+                Console.WriteLine(item.Age);
+            }
+            Console.ReadKey();
+          
 
         }
         public static void Register()
         {
-            Mapper.Initialize();
+            Mapper.Initialize(config =>
+            {
+                config.CreateMap<Person , PersonVM > ();
+            });
 
 
         }
